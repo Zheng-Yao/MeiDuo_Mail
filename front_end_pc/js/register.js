dad0bdccbeffece9c3a6aab1d86a312e5bf076db
Zheng-Yao <544806693@qq.com>
2018-06-20 21:43:21 +0800

    实现了图片验证码以及短信验证功能

--- front_end_pc/js/register.js
+++ front_end_pc/js/register.js
@@ -1,83 +1,106 @@
 var vm = new Vue({
-	el: '#app',
-	data: {
-		error_name: false,
-		error_password: false,
-		error_check_password: false,
-		error_phone: false,
-		error_allow: false,
-		error_image_code: false,
-		error_sms_code: false,
+    el: '#app',
+    data: {
+        error_name: false,
+        error_password: false,
+        error_check_password: false,
+        error_phone: false,
+        error_allow: false,
+        error_image_code: false,
+        error_sms_code: false,
 
-		username: '',
-		password: '',
-		password2: '',
-		mobile: '', 
-		image_code: '',
-		sms_code: '',
-		allow: false
-	},
-	methods: {
-		check_username: function (){
-			var len = this.username.length;
-			if(len<5||len>20) {
-				this.error_name = true;
-			} else {
-				this.error_name = false;
-			}
-		},
-		check_pwd: function (){
-			var len = this.password.length;
-			if(len<8||len>20){
-				this.error_password = true;
-			} else {
-				this.error_password = false;
-			}		
-		},
-		check_cpwd: function (){
-			if(this.password!=this.password2) {
-				this.error_check_password = true;
-			} else {
-				this.error_check_password = false;
-			}		
-		},
-		check_phone: function (){
-			var re = /^1[345789]\d{9}$/;
-			if(re.test(this.mobile)) {
-				this.error_phone = false;
-			} else {
-				this.error_phone = true;
-			}
-		},
-		check_image_code: function (){
-			if(!this.image_code) {
-				this.error_image_code = true;
-			} else {
-				this.error_image_code = false;
-			}	
-		},
-		check_sms_code: function(){
-			if(!this.sms_code){
-				this.error_sms_code = true;
-			} else {
-				this.error_sms_code = false;
-			}
-		},
-		check_allow: function(){
-			if(!this.allow) {
-				this.error_allow = true;
-			} else {
-				this.error_allow = false;
-			}
-		},
-		// 注册
-		on_submit: function(){
-			this.check_username();
-			this.check_pwd();
-			this.check_cpwd();
-			this.check_phone();
-			this.check_sms_code();
-			this.check_allow();
-		}
-	}
+        username: '',
+        password: '',
+        password2: '',
+        mobile: '',
+        image_code: '',
+        sms_code: '',
+        allow: false,
+        image_code_url:'',
+    },
+    //mounted:在模板渲染成html后调用，通常是初始化页面完成后，再对html的dom节点进行一些需要的操作。
+    mounted: function(){
+        this.get_image_code()
+    },
+    methods: {
+        //生成UUID
+        get_uuid: function () {
+            var d = new Date().getTime();
+            if (window.performance && typeof window.performance.now === "function") {
+                d += performance.now(); //use high-precision timer if available
+            }
+            var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+                var r = (d + Math.random() * 16) % 16 | 0;
+                d = Math.floor(d / 16);
+                return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+            });
+            return uuid;
+        },
+        //生成验证码
+        get_image_code: function () {
+            this.image_code_id = this.get_uuid();
+            this.image_code_url = "http://127.0.0.1:8000/image_code/" + image_code_id + '/';
+        },
+        check_username: function () {
+            var len = this.username.length;
+            if (len < 5 || len > 20) {
+                this.error_name = true;
+            } else {
+                this.error_name = false;
+            }
+        },
+        check_pwd: function () {
+            var len = this.password.length;
+            if (len < 8 || len > 20) {
+                this.error_password = true;
+            } else {
+                this.error_password = false;
+            }
+        },
+        check_cpwd: function () {
+            if (this.password != this.password2) {
+                this.error_check_password = true;
+            } else {
+                this.error_check_password = false;
+            }
+        },
+        check_phone: function () {
+            var re = /^1[345789]\d{9}$/;
+            if (re.test(this.mobile)) {
+                this.error_phone = false;
+            } else {
+                this.error_phone = true;
+            }
+        },
+        check_image_code: function () {
+            if (!this.image_code) {
+                this.error_image_code = true;
+            } else {
+                this.error_image_code = false;
+            }
+        },
+        check_sms_code: function () {
+            if (!this.sms_code) {
+                this.error_sms_code = true;
+            } else {
+                this.error_sms_code = false;
+            }
+        },
+        check_allow: function () {
+            if (!this.allow) {
+                this.error_allow = true;
+            } else {
+                this.error_allow = false;
+            }
+        },
+        // 注册
+        on_submit: function () {
+            this.check_username();
+            this.check_pwd();
+            this.check_cpwd();
+            this.check_phone();
+            this.check_sms_code();
+            this.check_allow();
+        }
+    }
 });
---
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -0,0 +1,59 @@
+import random
+from _testcapi import raise_exception
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+from django_redis import get_redis_connection
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from meiduo_mall.apps.verifications import serializers
+from meiduo_mall.apps.verifications.constants import MAX_TIME_IMAGE_CODE, SMS_TIME, TMP_ID, SEND_SMS_TIME
+from meiduo_mall.libs.captcha.captcha import captcha
+
+# 图片验证码
+from meiduo_mall.libs.yuntongxun.sms import CCP
+
+
+class ImageCodeView(APIView):
+    def get(self, request, image_code_id):
+        """获取图片验证码"""
+        text, image = captcha.generate_captcha()
+        # 获取redis连接对象
+        redis_conn = get_redis_connection('Image_code')
+        # 存储在redis中
+        redis_conn.setex('img_%s' % image_code_id, MAX_TIME_IMAGE_CODE, text)
+        print(redis_conn)
+        return HttpResponse(image, content_type='images/jpg')
+
+
+# GET/sms_codes/?\{moblie}/?image_code_id=xxxx&image_code_text=xxxx
+# 短信验证
+class SMSImageCode(GenericAPIView):
+    """
+        检查图片验证码
+        检查是否在60s内有发送记录
+        生成短信验证码
+        保存短信验证码与发送记录
+        发送短信
+    """
+    serializer_class = serializers.ImageCodeCheckSerializer
+
+    def get(self, request, moblie):
+        serializer = self.get_serializer(data=request.query_params)
+        serializer.is_valid(raise_exception=True)
+        # 随机生成6位手机验证码
+        moblie_code = '%06d' % random.randint(0, 999999)
+        # 验证完毕，进行redis批量操作,提高redis执行效率
+        redis_conn = get_redis_connection('Image_code')
+        p1 = redis_conn.pipeline()
+        p1.setex('send_flag_%s' % moblie, SEND_SMS_TIME, 1)
+        p1.setex('sms_%s' % moblie, SMS_TIME, moblie_code)
+        p1.execute()
+        ccp = CCP()
+        time = str(SMS_TIME / 60)
+        ccp.send_template_sms(moblie, [moblie_code, time], TMP_ID)
+        return Response({'message': 'OK'}, status.HTTP_200_OK)
